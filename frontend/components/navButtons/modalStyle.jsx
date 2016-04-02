75cae5ee1f0d4f2dcbfa11f8e551a2d2cf8ca00b
vadimvf <vadimfainberg@gmail.com>
2016-04-02 18:34:50 -0400

    Quick mobile fix pt 3

--- frontend/components/navButtons/modalStyle.jsx
+++ frontend/components/navButtons/modalStyle.jsx
@@ -10,7 +10,7 @@ var ModalStyle = {
   content : {
     display                    : "inline-block",
     position                   : 'absolute',
-    top                        : "5%",
+    top                        : "10%",
     left                       : "0",
     right                      : "0",
     border                     : "none",
