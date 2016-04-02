1a8d415fb21b525128ab07032c464ce0e5bef4cb
vadimvf <vadimfainberg@gmail.com>
2016-04-02 18:39:29 -0400

    Quick mobile fix final, login fits in screen

--- frontend/components/navButtons/modalStyle.jsx
+++ frontend/components/navButtons/modalStyle.jsx
@@ -10,7 +10,7 @@ var ModalStyle = {
   content : {
     display                    : "inline-block",
     position                   : 'absolute',
-    top                        : "20%",
+    top                        : "25%",
     left                       : "0",
     right                      : "0",
     border                     : "none",
